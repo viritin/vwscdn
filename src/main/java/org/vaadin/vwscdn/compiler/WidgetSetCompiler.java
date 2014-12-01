daf4dd09ca6db612851e8dea26e7cc96eb5f7495
Sami Ekblad <sami@vaadin.com>
2014-12-02 01:08:51 +0200

    Code clean

--- src/main/java/org/vaadin/vwscdn/compiler/WidgetSetCompiler.java
+++ src/main/java/org/vaadin/vwscdn/compiler/WidgetSetCompiler.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.vaadin.vwscdn.compiler;
 
 import java.io.BufferedReader;
@@ -19,8 +14,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.tools.JavaCompiler;
-import javax.tools.ToolProvider;
 import org.vaadin.vwscdn.server.WSCompilerService;
 import static org.vaadin.vwscdn.server.WSCompilerService.COMPILER_ROOT_DIR;
 import org.vaadin.vwscdn.server.WidgetSetServlet;
