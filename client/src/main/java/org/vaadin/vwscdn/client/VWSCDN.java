b722a3913b8a4b77cda9c55d82673017ce1dff24
Sami Ekblad <sami@vaadin.com>
2014-12-13 08:55:20 +0200

    The service can be run in local-mode only by using -Dvwscdn.local

--- client/src/main/java/org/vaadin/vwscdn/client/VWSCDN.java
+++ client/src/main/java/org/vaadin/vwscdn/client/VWSCDN.java
@@ -20,17 +20,23 @@ import org.jsoup.nodes.Element;
 class VWSCDN {
 
     public static final String COMPILE_SERVICE_URL = "http://sami.app.fi/rws";
+    public static final String COMPILE_SERVICE_URL_LOCAL = "http://localhost:8080/vwscdn";
+    public static final String PARAM_VWSCDN_LOCAL = "vwscdn.local";
+
+    private static String getDefaultServiceUrl() {
+        return System.getProperty(PARAM_VWSCDN_LOCAL) != null ? COMPILE_SERVICE_URL_LOCAL : COMPILE_SERVICE_URL;
+    }
 
     private Client client;
     private WebTarget target;
     private final VaadinService service;
 
     public VWSCDN(VaadinService service) {
-        this(service, COMPILE_SERVICE_URL);
+        this(service, getDefaultServiceUrl());
     }
 
     public VWSCDN(VaadinService service, String vwscdnUrl) {
-        vwscdnUrl = vwscdnUrl == null ? COMPILE_SERVICE_URL : vwscdnUrl;
+        vwscdnUrl = vwscdnUrl == null ? getDefaultServiceUrl() : vwscdnUrl;
         this.client = ClientBuilder.newClient();
         vwscdnUrl = vwscdnUrl.endsWith("/") ? vwscdnUrl : vwscdnUrl + "/";
         this.target = client.target(vwscdnUrl + "api/compiler/compile");
