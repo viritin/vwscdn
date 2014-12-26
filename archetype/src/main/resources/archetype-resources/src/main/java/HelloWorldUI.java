0a07b5d4590ff5d9366013de44571bfd3b232307
Sami Ekblad <sami@vaadin.com>
2014-12-26 14:15:19 +0200

    Added archetype module

---
+++ archetype/src/main/resources/archetype-resources/src/main/java/HelloWorldUI.java
@@ -0,0 +1,34 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@Theme("valo")
+@SuppressWarnings("serial")
+public class HelloWorldUI extends UI {
+
+    @WebServlet(value = {"/*"}, asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = HelloWorldUI.class)
+    public static class Servlet extends VaadinServlet {    
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        setContent(layout);
+        layout.addComponent(new Label("Hello World!"));
+    }
+
+}
+
