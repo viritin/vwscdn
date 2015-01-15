af69010f13508668dab475fc6d0584657343c80f
Sami Ekblad <sami@vaadin.com>
2015-01-15 20:49:25 +0200

    Support for asynchronous compilation during build. This fixes #9.

--- maven-plugin/src/main/java/in/virit/vwscdn/VWSCDNMojo.java
+++ maven-plugin/src/main/java/in/virit/vwscdn/VWSCDNMojo.java
@@ -15,6 +15,11 @@ package in.virit.vwscdn;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import in.virit.vwscdn.client.AddonInfo;
+import in.virit.vwscdn.client.Connection;
+import in.virit.vwscdn.client.PublishState;
+import in.virit.vwscdn.client.WidgetSetRequest;
+import in.virit.vwscdn.client.WidgetSetResponse;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -24,7 +29,6 @@ import org.apache.maven.plugin.MojoExecutionException;
 
 import java.net.URL;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -54,6 +58,22 @@ public class VWSCDNMojo
     private MavenProject project;
 
     /**
+     * Should the compilation be requested as asynchronous. This is the default,
+     * but if synchronous mode is needed this should be set to false.
+     *
+     */
+    @Parameter(property = "vwscdn.async", defaultValue = "true", readonly = true)
+    private boolean asyncCompile;
+
+    /**
+     * Compilation style for widget set. 
+     * Default is the "OBF". Supported values "OBF", "PRETTY", "DETAILED"
+     *
+     */
+    @Parameter(property = "vwscdn.compile.style", defaultValue = "OBF", readonly = true)
+    private String compileStyle;
+
+    /**
      * Output directory for generated source files.
      *
      */
@@ -64,6 +84,8 @@ public class VWSCDNMojo
             throws MojoExecutionException {
 
         String className = "GeneratedWidgetSet";
+
+        String vaadinVersion = null;
 
         // Use same package as Maven plugin
         File packageDirectory = new File(outputDirectory,
@@ -81,24 +103,8 @@ public class VWSCDNMojo
             Map<String, URL> availableWidgetSets = ClassPathExplorer
                     .getAvailableWidgetSets(urls);
             Set<Artifact> artifacts = project.getArtifacts();
-
-            out.write(
-                    "package " + VWSCDNMojo.class.getPackage().getName() + ";\n\n");
-            out.write("import in.virit.vwscdn.client.DefaultWidgetSet;\n");
-            out.write("import in.virit.vwscdn.annotations.WidgetSet;\n"
-                    + "import javax.servlet.annotation.WebListener;\n"
-                    + "import javax.servlet.http.HttpSessionEvent;\n");
-            out.write(
-                    "import static in.virit.vwscdn.annotations.WidgetSetType.GENERATED;\n");
-            out.write("\n@WebListener\n");
-            out.write("@WidgetSet(GENERATED)\n");
-            out.write(
-                    "public class " + className + " extends DefaultWidgetSet implements javax.servlet.http.HttpSessionListener {\n"
-                    + "    private boolean inited = false;\n"
-                    + "\n"
-                    + "    public " + className + "() { \n"
-                    + "        super(); \n");
-            Set<Artifact> requiredArtifacts = new HashSet<>();
+            //Set<Artifact> requiredArtifacts = new HashSet<>();
+            WidgetSetRequest wsReq = new WidgetSetRequest();
             for (String name : availableWidgetSets.keySet()) {
                 URL url = availableWidgetSets.get(name);
                 for (Artifact a : artifacts) {
@@ -106,23 +112,86 @@ public class VWSCDNMojo
                     if (u.contains(a.getArtifactId())
                             && u.contains(a.getBaseVersion()) && !u.contains(
                                     "vaadin-client")) {
-                        requiredArtifacts.add(a);
+                        wsReq.addon(a.getGroupId(), a.getArtifactId(), a.getBaseVersion());
+                    }
+                    
+                    // Store the vaadin version
+                    if (a.getArtifactId().equals("vaadin-server")) {
+                        vaadinVersion = a.getVersion();
                     }
                 }
             }
-            for (Artifact a : requiredArtifacts) {
+
+            // Request compilation for the widgetset   
+            wsReq.setCompileStyle(compileStyle);
+            wsReq.setVaadinVersion(vaadinVersion);
+            String wsName = null;
+            String wsUrl = null;
+
+            Connection conn = new Connection();
+            WidgetSetResponse wsRes = conn.queryRemoteWidgetSet(wsReq, asyncCompile);
+            if (wsRes != null && (wsRes.getStatus() == PublishState.AVAILABLE // Compiled and published
+                    || wsRes.getStatus() == PublishState.COMPILED // Compiled succesfully, but not yet available
+                    || wsRes.getStatus() == PublishState.COMPILING)) // Currently compiling the widgetset)
+            {
+                wsName = wsRes.getWidgetSetName();
+                wsUrl = wsRes.getWidgetSetUrl();
+            } else {
+                throw new MojoExecutionException("Remote widgetset compilation failed: " + (wsRes != null? wsRes.getStatus(): " (no response)"));
+            }
+
+            out.write(
+                    "package " + VWSCDNMojo.class.getPackage().getName() + ";\n\n");
+
+            out.write("import in.virit.vwscdn.client.DefaultWidgetSet;\n");
+            out.write("import in.virit.vwscdn.client.PublishState;\n");
+            out.write("import in.virit.vwscdn.client.WidgetSetResponse;\n");
+            out.write("import in.virit.vwscdn.annotations.WidgetSet;\n"
+                    + "import javax.servlet.annotation.WebListener;\n"
+                    + "import javax.servlet.http.HttpSessionEvent;\n");
+            out.write(
+                    "import static in.virit.vwscdn.annotations.WidgetSetType.GENERATED;\n");
+
+            out.write("\n@WebListener\n");
+            out.write("@WidgetSet(GENERATED)\n");
+            out.write(
+                    "public class " + className + " extends DefaultWidgetSet implements javax.servlet.http.HttpSessionListener {\n"
+                    + "    private boolean inited = false;\n"
+                    + "\n");
+            if (wsName != null && wsUrl != null) {
+                out.write("    private WidgetSetResponse wsr = new WidgetSetResponse();\n");
+            } else {
+                out.write("    private WidgetSetResponse wsr = null;\n");
+            }
+            out.write("\n"
+                    + "    public " + className + "() { \n"
+                    + "        super(); \n");
+
+            for (AddonInfo a : wsReq.getAddons()) {
                 String aid = a.getArtifactId();
                 String gid = a.getGroupId();
-                String v = a.getBaseVersion();
+                String v = a.getVersion();
                 out.write("\n        addon(\""
                         + gid + "\", \"" + aid + "\", \"" + v + "\");");
             }
+
+            if (wsName != null && wsUrl != null) {
+                // We assume the WS to be available by the time it is requested
+                out.write("\n        wsr.setStatus(PublishState.AVAILABLE);");
+                out.write("\n        wsr.setWidgetSetUrl(\"" + wsUrl + "\");");
+                out.write("\n        wsr.setWidgetSetName(\"" + wsName + "\");");
+            }
+
             out.write("\n"
                     + "    }\n");
             out.write("\n    @Override\n"
                     + "    public void sessionCreated(HttpSessionEvent se) {\n"
                     + "        if(!inited) {\n"
-                    + "            init();\n"
+                    + "            if(wsr == null) {\n"
+                    + "               init();\n"
+                    + "            } else {\n"
+                    + "               initWithResponse(wsr);\n"
+                    + "            }\n"
                     + "            inited = true;\n"
                     + "        }\n"
                     + "    }\n"
@@ -134,10 +203,17 @@ public class VWSCDNMojo
 
             out.write("}\n");
 
+            // Print some info            
             System.out.println(
-                    requiredArtifacts.size() + " addons widget set found.");
-            System.out.println("Widget Set created to " + outputFile.
-                    getAbsolutePath() + ".");
+                    wsReq.getAddons().size() + " addons widget set found.");
+            if (wsName != null && wsUrl != null) {
+                System.out.println("Widget set '" + wsName + "' created to " + outputFile.
+                        getAbsolutePath() + ". Widgetset available at: " + wsUrl);
+            } else {
+                System.out.println("Widget set created to " + outputFile.
+                        getAbsolutePath() + ".");
+
+            }
 
             project.addCompileSourceRoot("target/generated-sources/vwscdn");
         } catch (DependencyResolutionRequiredException | MalformedURLException ex) {
