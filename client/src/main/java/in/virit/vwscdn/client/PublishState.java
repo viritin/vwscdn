b912619a1df2c08805fdae3b8318231d8dfe949b
Sami Ekblad <sami@vaadin.com>
2015-01-16 20:26:10 +0200

    refactored compilation

--- client/src/main/java/in/virit/vwscdn/client/PublishState.java
+++ client/src/main/java/in/virit/vwscdn/client/PublishState.java
@@ -1,11 +1,13 @@
 package in.virit.vwscdn.client;
 
 public enum PublishState {
+
     UNKNOWN, // We don't have idea yet
     NOT_FOUND, // No widgetset exists with an id and has not been compiled
     AVAILABLE, // Compiled and published
     COMPILED, // Compiled succesfully, but not yet available
     COMPILING, // Currently compiling the widgetset
+    QUEUED, // Waiting for compilation
     ERROR; // Widgetset was compiled, but error occured
 
 }
