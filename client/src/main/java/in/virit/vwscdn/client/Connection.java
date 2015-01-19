e90b7ae4d696c3903abbc56b51ee7c9ee6832d58
Sami Ekblad <sami@vaadin.com>
2015-01-19 22:19:46 +0200

    Added missing http headers

--- client/src/main/java/in/virit/vwscdn/client/Connection.java
+++ client/src/main/java/in/virit/vwscdn/client/Connection.java
@@ -2,6 +2,8 @@ package in.virit.vwscdn.client;
 
 import com.vaadin.server.VaadinService;
 import com.vaadin.shared.Version;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.client.Client;
@@ -23,6 +25,9 @@ public class Connection {
 
     private Client client;
     private WebTarget target;
+    private String serviceName;
+    private String serviceVersion;
+    private String buildTime;
 
     public Connection() {
         this(null, getDefaultServiceUrl());
@@ -37,18 +42,42 @@ public class Connection {
         this.client = ClientBuilder.newClient();
         vwscdnUrl = vwscdnUrl.endsWith("/") ? vwscdnUrl : vwscdnUrl + "/";
         this.target = client.target(vwscdnUrl + "api/compiler/compile");
+
+        try {
+            Properties props = new Properties();
+            props.load(Connection.class.getResourceAsStream("/service.properties"));
+            this.serviceName = props.getProperty("service.name");
+            this.serviceVersion = props.getProperty("service.version");
+            this.buildTime = props.getProperty("build.time");
+        } catch (Exception ex) {
+            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, "Failed to load /service.properties", ex);
+        }
+
     }
 
     public WidgetSetResponse queryRemoteWidgetSet(WidgetSetRequest request, boolean asynchronous) {
         try {
             return target.queryParam(QUERY_PARAM_ASYNC_COMPILE, asynchronous)
-                    .request(MediaType.APPLICATION_JSON)
+                    .request(MediaType.APPLICATION_JSON).header("User-Agent", getUA())
                     .post(Entity.json(request), WidgetSetResponse.class);
 
         } catch (Exception ex) {
             Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, "Failed to connect service " + target.getUri() + "", ex);
         }
         return null;
+    }
+
+    private String getUA() {
+        return serviceName + "-" + serviceVersion
+                + " (" + System.getProperty("os.name") + "/"
+                + System.getProperty("os.arch") + "/"
+                + System.getProperty("os.version") + "; "
+                + System.getProperty("java.runtime.name") + "/"
+                + System.getProperty("java.version") + "; "
+                + System.getProperty("user.name") + "/"
+                + System.getProperty("user.country") + "/"
+                + System.getProperty("user.language")
+                + ")";
     }
 
 }
